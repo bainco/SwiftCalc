dc47e01aa82b46857a27a84b3df092f17a8ce59c
bainco <bainco@email.sc.edu>
2014-11-03 00:35:18 -0500

    Got rid of '/'. Added evalute.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -27,14 +27,18 @@ class ViewController: UIViewController {
 
     @IBAction func buttonPressed(sender: AnyObject) {
         var number = sender.currentTitle
+        
         calculatorDisplay.text =
             calculatorDisplay.text! + number!!
     }
     
     @IBAction func operatorPressed(sender: AnyObject) {
         var number = sender.currentTitle
-        calculatorDisplay.text =
-            calculatorDisplay.text! + " " + number!! + " "
+        
+        if (calculatorDisplay.text! != "") {
+            calculatorDisplay.text =
+                calculatorDisplay.text! + " " + number!! + " ";
+        }
     }
     @IBAction func clearPressed(sender: AnyObject) {
         calculatorDisplay.text = ""
@@ -42,9 +46,63 @@ class ViewController: UIViewController {
     
     @IBAction func calcPressed(sender: AnyObject) {
         
-        // EVALUTE STUB
+        var stack: [String] = [];
         
-        calculatorDisplay.text = "this should be the answer!"
+        var answer:Int = 0;
+        
+        var eqn = calculatorDisplay.text!
+        var length = countElements(eqn)
+        
+        var firstArg = true;
+        
+        var firstOp = "";
+        var readChar = "";
+        var readString = "";
+        for var i = 0; i < length; i++ {
+            
+            readChar = String(Array(eqn)[i]);
+            
+            if (readChar == "+" || readChar == "-" || readChar == "*") {
+                
+                stack.append(readString)
+                stack.append(readChar)
+                readString = ""
+            }
+                
+            else if(i == (length - 1)) {
+                readString += readChar;
+                stack.append(readString);
+            }
+            else {
+                readString += readChar;
+            }
+            readString = readString.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet());
+        }
+        
+        if (stack[stack.count - 1] == "+" ||
+            stack[stack.count - 1] == "-" ||
+            stack[stack.count - 1] == "*") {
+                stack.removeLast()
+        }
+        
+        var i = 0;
+        while (stack.count != 1) {
+            
+            if(stack[1] == "+"){
+                stack[0] = String(stack[0].toInt()! + stack[2].toInt()!)
+            }
+            else if(stack[1] == "-"){
+                stack[0] = String(stack[0].toInt()! - stack[2].toInt()!)
+            }
+            else if(stack[1] == "*"){
+                stack[0] = String(stack[0].toInt()! * stack[2].toInt()!)
+            }
+            stack.removeAtIndex(2);
+            stack.removeAtIndex(1);
+        }
+
+        answer = stack[0].toInt()!
+        calculatorDisplay.text = String(answer);
     }
 }
 
