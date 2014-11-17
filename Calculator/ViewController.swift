26234966cd0e5484e872f2be435cc08ace4f1c86
trevorhutto <huttotw@gmail.com>
2014-11-17 10:26:01 -0500

    Comments and such

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Calculator
 //
-//  Created by Trevor W. Hutto on 10/30/14.
+//  Created by Trevor W. Hutto, Connor Bain and Andrew Freix on 10/30/14.
 //  Copyright (c) 2014 trevorhutto. All rights reserved.
 //
 
@@ -36,6 +36,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func operatorPressed(sender: AnyObject) {
+        // Check to make sure we don't have two operators.
         if(self.operatorPressed == false) {
             self.operatorPressed = true
             var operatorPressed = sender.currentTitle
@@ -49,7 +50,11 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calcPressed(sender: AnyObject) {
-        self.operatorPressed = false 
+        self.operatorPressed = false
+        
+        // Intialize local variables.
+        var firstArg = true;
+        
         var stack: [String] = []
         
         var answer:Int = 0;
@@ -57,11 +62,10 @@ class ViewController: UIViewController {
         var eqn = calculatorDisplay.text!
         var length = countElements(eqn)
         
-        var firstArg = true;
+        var firstOp:String = ""
+        var readChar:String = ""
+        var readString:String = ""
         
-        var firstOp = ""
-        var readChar = ""
-        var readString = "";
         for var i = 0; i < length; i++ {
             
             readChar = String(Array(eqn)[i]);
@@ -83,22 +87,32 @@ class ViewController: UIViewController {
             readString = readString.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet());
         }
         
+        // We don't want to end with an operator.
         if (stack[stack.count - 1] == "+" ||
             stack[stack.count - 1] == "-" ||
             stack[stack.count - 1] == "*") {
                 stack.removeLast()
         }
+        
+        // Start the evaluation
+        
+        // Multiplication first.
         var i = 0;
         while (i < stack.count){
             if(stack[i] == "*"){
+                
+                // Replace the multiplication with the answer.
                 stack[i - 1] = String(stack[i - 1].toInt()! * stack[i + 1].toInt()!)
                 i--
+                
+                // Already evaluated.
                 stack.removeAtIndex(i+2)
                 stack.removeAtIndex(i+1)
             }
             i++
         }
         
+        // Finish it with addition and subtraction.
         i = 0;
         while (stack.count != 1) {
             
@@ -108,11 +122,16 @@ class ViewController: UIViewController {
             else if(stack[1] == "-"){
                 stack[0] = String(stack[0].toInt()! - stack[2].toInt()!)
             }
+            
+            // Already evaluated.
             stack.removeAtIndex(2);
             stack.removeAtIndex(1);
         }
 
+        // The answer is now the first element.
         answer = stack[0].toInt()!
+        
+        // Output it to the display.
         calculatorDisplay.text = String(answer);
     }
 }
